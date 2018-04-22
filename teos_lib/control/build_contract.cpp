c64f13e996a8700c6547aada3d2a3d3f240639b4
sz <stefan.zarembinski@gmail.com>
2018-04-22 10:54:38 +0200

    generate abi

--- teos_lib/control/build_contract.cpp
+++ teos_lib/control/build_contract.cpp
@@ -4,41 +4,45 @@
 #include <string>
 #include <vector>
 
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/process.hpp>
 #include <boost/filesystem.hpp>
-
-#include <teoslib/control/config.hpp>
-#include <teoslib/control/build_contract.hpp>
 #include <boost/algorithm/string/predicate.hpp>
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string/classification.hpp>
+
+#include <teoslib/control/config.hpp>
+#include <teoslib/control/build_contract.hpp>
 
 using namespace std;
 
 namespace teos {
   namespace control {
 
-    void generateAbi(
-      string typesHpp,
-      string targetAbiFile,
-      string includeDir // comma separated list of include dirs
+    void GenerateAbi::generateAbi(
+      string types_hpp,
+      string target_abi_file,
+      string include_dir // comma separated list of include dirs
     )
     {
       namespace bfs = boost::filesystem;
 
-      bfs::path typesHppPath(typesHpp);
-      string name = typesHppPath.stem().string();
-      bfs::path contextFolder = path.parent().string();
-      bfs::path output(targetAbiFile);
-      if(targetAbiFile.empty()){
-        output = contextFolder / (name + ".abi")
+      bfs::path types_pth(types_hpp);
+      string name = types_pth.stem().string();
+      bfs::path contextFolder = types_pth.parent_path();
+
+      bfs::path target_path(target_abi_file);
+      if(target_abi_file.empty()){
+        target_path = contextFolder / (name + ".abi");
       } else {
-        output = bfs::path(targetAbiFile);
-        if(!output.is_absolute()){
-          output = contextFolder / output;
+        target_path = bfs::path(target_abi_file);
+        if(!target_path.is_absolute()){
+          target_path = contextFolder / target_path;
         }
       }
 
-      string commandLine = getSourceDir(this) 
+      string command_line = getSourceDir(this) 
         + "/build/programs/eosio-abigen/eosio-abigen"
         + " -extra-arg=-c -extra-arg=--std=c++14 -extra-arg=--target=wasm32"
         + " -extra-arg=-nostdinc -extra-arg=-nostdinc++ -extra-arg=-DABIGEN"
@@ -48,40 +52,63 @@ namespace teos {
         + " -extra-arg=-I" + getSourceDir(this) + "/contracts"
         + " -extra-arg=-I" + contextFolder.string();
 
-      if(!includeDir.empty())
+      if(!include_dir.empty())
       {
         vector<string> includeDirs;
-        boost::split(includeDirs, includeDir, boost::algorithm::is_any_of(","));
+        boost::split(includeDirs, include_dir, boost::algorithm::is_any_of(","));
         for (string dir : includeDirs) {
-          commandLine += " -extra-arg=-I" + dir;
+          command_line += " -extra-arg=-I" + dir;
         }
       }
 
-      commandLine
-        += " -extra-arg=-fparse-all-comments"
-        + " -destination-file=" + outname.string()
+      command_line = command_line
+        + " -extra-arg=-fparse-all-comments"
+        + " -destination-file=" + target_path.string()
         + " -verbose=0"
-        + " -context=" + contextFolder
-        + typesHppPath.string() + " --";
+        + " -context=" + contextFolder.string()
+        + " " + types_pth.string() + " --";
+      
+      //cout << command_line << endl;
 
-      boostProcessSystem(commandLine);
+      {
+        namespace bp = boost::process;
+        namespace pt = boost::property_tree;
+
+        bp::ipstream err;
+        bp::child c(command_line, bp::std_err > err);
+
+        string err_line;
+        string error_msg;
+        while (c.running() && std::getline(err, err_line) && !err_line.empty()){
+            error_msg += err_line + "\n";
+        }
+        c.wait();
+
+        if(!error_msg.empty()){
+          putError(error_msg);
+        } else {
+          pt::read_json(target_path.string(), respJson_);
+          //cout << responseToString();
+        }
+      }
     };
 
     void wasmClangHelp()
     {
-      string commandLine;
-      commandLine += getWASM_CLANG(nullptr) + " --help";
-      boostProcessSystem(commandLine);
+      string command_line;
+      command_line += getWASM_CLANG(nullptr) + " --help";
+      boostProcessSystem(command_line);
     }
 
 
     /*
-    See a basic example of the build procedure: https://gist.github.com/yurydelendik/4eeff8248aeb14ce763e#example.
+    See a basic example of the build procedure: 
+      https://gist.github.com/yurydelendik/4eeff8248aeb14ce763e#example.
     */
     void BuildContract::buildContract(
       string src, // comma separated list of source c/cpp files
-      string targetWastFile,
-      string includeDir // comma separated list of include dirs
+      string target_wast_file,
+      string include_dir // comma separated list of include dirs
     )
     {
       namespace bfs = boost::filesystem;
@@ -95,18 +122,19 @@ namespace teos {
       vector<string> srcs;
       boost::split(srcs, src, boost::algorithm::is_any_of(","));
       string objectFileList;
+      bfs::path target_path(target_wast_file);
+
       for (string file : srcs)
       {
         bfs::path srcFile(file);
         string name = srcFile.stem().string();
 
-        bfs::path outputWast(targetWastFile)
-        if(outputWast.empty()){
-          outputWast = name + "wast";
+        if(target_wast_file.empty()){
+          target_path = name + "wast";
         } else {
-          outputWast = bfs::path(targetAbiFile);
-          if(!outputWast.is_absolute()){
-            outputWast = contextFolder / outputWast;
+          target_path = bfs::path(target_wast_file);
+          if(!target_path.is_absolute()){
+            target_path = srcFile.parent_path() / target_path;
           }          
         }
 
@@ -114,8 +142,8 @@ namespace teos {
         bfs::path output(build / (name + ".o"));
         objectFileList += output.string() + " ";
 
-        string commandLine;
-        commandLine += getWASM_CLANG(this)
+        string command_line;
+        command_line += getWASM_CLANG(this)
           + " -emit-llvm -O3  --std=c++14  --target=wasm32 -nostdinc -nostdlib"
           + " -nostdlibinc -ffreestanding -nostdlib -fno-threadsafe-statics"
           + " -fno-rtti -fno-exceptions"
@@ -126,64 +154,64 @@ namespace teos {
           + " -I " + srcFile.parent_path().string();
 
         vector<string> includeDirs;
-        boost::split(includeDirs, includeDir, boost::algorithm::is_any_of(","));
+        boost::split(includeDirs, include_dir, boost::algorithm::is_any_of(","));
         for (string dir : includeDirs) {
-          commandLine += " -I " + dir;
+          command_line += " -I " + dir;
         }
 
-        commandLine += " -c " + file
+        command_line += " -c " + file
           + " -o " + output.string();
 
-        cout << commandLine << endl;
-        boostProcessSystem(commandLine);
+        cout << command_line << endl;
+        boostProcessSystem(command_line);
       }
 
       string linked = workdir.string() + "/linked.bc";
       {
-        string commandLine;
-        commandLine += getWASM_LLVM_LINK(this)
+        string command_line;
+        command_line += getWASM_LLVM_LINK(this)
           + " -o " + linked
           + " " + objectFileList;
-        cout << commandLine << endl;
+        cout << command_line << endl;
         /*
         /home/cartman/opt/wasm/bin/llvm-link
         -o /tmp/tmp.fXlBIIodY4/linked.bc
         /tmp/tmp.fXlBIIodY4/built/skeleton.cpp
         */
-        boostProcessSystem(commandLine);
+        boostProcessSystem(command_line);
       }
 
       string assembly = workdir.string() + "/assembly.s";
       {
-        string commandLine;
-        commandLine += getWASM_LLC(nullptr)
+        string command_line;
+        command_line += getWASM_LLC(nullptr)
           + " --asm-verbose=false"
           + " -o " + assembly
           + " " + linked;
-        cout << commandLine << endl;
+        cout << command_line << endl;
         /*
         /home/cartman/opt/wasm/bin/llc
         --asm-verbose=false
         -o /tmp/tmp.fXlBIIodY4/assembly.s
         /tmp/tmp.fXlBIIodY4/linked.bc
         */
-        boostProcessSystem(commandLine);
+        boostProcessSystem(command_line);
       }
 
       {
-        string commandLine;
-        commandLine += getBINARYEN_BIN(nullptr) + "/s2wasm"
-          + " -o " + outputWast.string()
+        string command_line;
+        command_line += getBINARYEN_BIN(nullptr) + "/s2wasm"
+          + " -o " + target_path.string()
           + " -s 16384"
           + " " + assembly;
-        cout << commandLine << endl;
+        cout << command_line << endl;
         /*
         /home/cartman/opt/binaryen/bin/s2wasm
         -o /tmp/hello.wast
         -s 16384
         /tmp/tmp.fXlBIIodY4/assembly.s
         */
-        boostProcessSystem(commandLine);
+        boostProcessSystem(command_line);
       }
 
       bfs::remove_all(workdir);
