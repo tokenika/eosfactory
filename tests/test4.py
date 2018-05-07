5812d15f9b11b1abb991fe46324d896791012139
stefanzarembinski <stefan.zarembinski@gmail.com>
2018-05-07 08:23:10 +0200

    tic-tac-toe

--- tests/test4.py
+++ tests/test4.py
@@ -3,19 +3,19 @@ import sess
 from eosf import *
 
 def run():
-    node.reset()
-    sess.init()
+node.reset()
+sess.init()
 
-    c = Contract("currency")
-    c.get_code()
+c = Contract("currency")
+c.get_code()
 
-    c.abi()
-    c.wast()
+c.abi()
+c.wast()
 
-    c.abi()
+c.abi()
 
-    #print("Test OK")
-    node.stop()
+#print("Test OK")
+node.stop()
 
 if __name__ == "__main__":
     run()
