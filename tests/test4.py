075f96b0f730f3271c9cca14cd60a7930663919a
jakub-zarembinski <jakub.zarembinski@tokenika.io>
2018-05-15 13:24:49 +0200

    test4 fixed

--- tests/test4.py
+++ tests/test4.py
@@ -1,20 +1,12 @@
 # python3 ./tests/test4.py
 
-import time
 import node
 import sess
 from eosf import *
 
-"""
-NOTE: This test fails due to an unresolved bug
-"""
-
 def run():
     print('test node.reset():')
     node.reset()
-
-    print('test node.info():')
-    node.info()
 
     print('test sess.init():')
     sess.init()
@@ -22,19 +14,11 @@ def run():
     print('test Contract("eosio.token"):')
     c = Contract("eosio.token")
 
-    print('test c.get_code():')
-    c.get_code()
-
     print('test c.deploy():')
     c.deploy()
 
-    print('test c.get_code():')
-    c.get_code()
-
     print('test c.push_action("create"):')
     c.push_action("create", '{"issuer":"eosio", "maximum_supply":"1000000000.0000 EOS", "can_freeze":0, "can_recall":0, "can_whitelist":0}')
-
-    time.sleep(1)
     
     print('test c.show_action("create"):')
     c.show_action("create", '{"issuer":"eosio", "maximum_supply":"1000000000.0000 EOS", "can_freeze":0, "can_recall":0, "can_whitelist":0}')
