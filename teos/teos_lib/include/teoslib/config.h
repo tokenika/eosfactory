b14922148c75bc7cd72cb1c435ca6ca555cf737d
stefanzarembinski <stefan.zarembinski@gmail.com>
2018-05-18 17:41:34 +0200

    cleaned cmake lists

--- teos/teos_lib/include/teoslib/config.h
+++ teos/teos_lib/include/teoslib/config.h
@@ -4,8 +4,8 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define CONFIG_JSON "config.json"
-#define TEST_ADDRESS "tokenika"
-#define TEST_HTTP_ADDRESS "eos-testapi.tokenika.io:8888"
+#define TEST_ADDRESS ""
+#define TEST_HTTP_ADDRESS ""
 #define USE_CONFIG_JSON "config.json"
 #define LOCALHOST_ADDRESS "localhost"
 #define LOCALHOST_HTTP_ADDRESS "127.0.0.1:8888"
