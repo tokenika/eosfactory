16b0dbdfb3a1ab32530b20e7be932926244bbe65
jakub-zarembinski <jakub.zarembinski@tokenika.io>
2018-08-27 18:56:25 +0200

    Contract inheriting from ContractBuilder

--- pyteos/eosf_contract.py
+++ pyteos/eosf_contract.py
@@ -1,16 +1,8 @@
-import json
-import inspect
-import types
-import time
-import re
+import shutil
 
-import setup
 import front_end
 import teos
 import cleos
-import cleos_system
-import eosf
-import eosf_account
 
 
 def contract_workspace_from_template(
@@ -31,16 +23,15 @@ def contract_workspace_from_template(
     logger = front_end.Logger(verbosity)
 
     logger.INFO('''
-    ######### Create template ``{}`` contract workspace named ``{}``.
+    ######### Create contract ``{}`` from template ``{}``.
     '''.format(name, template))
 
     result = teos.TemplateCreate(
         name, template, user_workspace, remove_existing, visual_studio_code, is_verbose=0)
 
-
     if not logger.ERROR(result):
-        logger.TRACE('''
-        * The directory is
+        logger.INFO('''
+        * Contract directory is
             {}
         '''.format(result.contract_path_absolute))
         return result.contract_path_absolute
@@ -53,27 +44,20 @@ class ContractBuilder(front_end.Logger):
     '''
     def __init__(
             self, contract_dir,
-            is_mutable = True,
             verbosity=None):
-
-        self.contract_dir = contract_dir
-        self.is_mutable = is_mutable
-
-        front_end.Logger.__init__(self, verbosity)
-
-        if (not verbosity):
-            self.INFO('''
-                    ######### Create a ``ContractBuilder`` object.
-                    ''')
-            config = teos.GetConfig(self.contract_dir, is_verbose=0)
-            self.contract_dir = config.json["contract-dir"]
-            if not self.contract_dir:
-                self.ERROR("""
-                    Cannot determine the contract directory. The clue is 
-                    ``{}``.
-                    """.format(contract_dir))
-                return
-            self.INFO('''
+        super().__init__(verbosity)
+        self.INFO('''
+                ######### Create a ``Contract`` object.
+                ''')
+        config = teos.GetConfig(contract_dir, is_verbose=0)
+        self.contract_dir = config.json["contract-dir"]
+        if not self.contract_dir:
+            self.ERROR("""
+                Cannot determine the contract directory. The path is 
+                ``{}``.
+                """.format(contract_dir))
+            return
+        self.INFO('''
             * Contract directory is
                 {}
             '''.format(self.contract_dir))
@@ -82,39 +66,40 @@ class ContractBuilder(front_end.Logger):
         return self.contract_dir
 
     def build_wast(self, json=False):
-        if self.is_mutable:
-            result = teos.WAST( 
-                self.contract_dir, "", is_verbose=0, json=json)
-            
-            if not self.ERROR(result):
-                self.INFO('''
-                * WAST file build and saved.
-                ''')
-            if json:
-                return result.json
-        else:
-            self.ERROR("Cannot modify system contracts.")
+        result = teos.WAST( 
+            self.contract_dir, "", is_verbose=0, json=json)
+        
+        if not self.ERROR(result):
+            self.INFO('''
+            * WAST file build and saved.
+            ''')
+        if json:
+            return result.json
 
     def build_abi(self, json=False):
-        if self.is_mutable:
-            result = teos.ABI(self.contract_dir, "", is_verbose=0)
-            if not self.ERROR(result):
-                self.INFO('''
-                * ABI file build and saved.
-                ''')
-                if "ABI exists in the source directory" in result.out_msg:
-                   self.TRACE(result.out_msg)
-                if json:
-                    return result.json
-        else:
-            self.ERROR("Cannot modify system contracts.")
+        result = teos.ABI(self.contract_dir, "", is_verbose=0)
+        if not self.ERROR(result):
+            self.INFO('''
+            * ABI file build and saved.
+            ''')
+            if "ABI exists in the source directory" in result.out_msg:
+                self.TRACE(result.out_msg)
+            if json:
+                return result.json
 
     def build(self):
         self.build_abi()
         self.build_wast()
 
+    def delete(self):
+        try:
+            shutil.rmtree(str(self.contract_dir))
+            return True
+        except:
+            return False
 
-class Contract(front_end.Logger):
+
+class Contract(ContractBuilder):
 
     def __init__(
             self, account, contract_dir,
@@ -125,9 +110,8 @@ class Contract(front_end.Logger):
             max_cpu_usage=0, max_net_usage=0,
             ref_block=None,
             verbosity=None):
-
+        super().__init__(contract_dir, verbosity)
         self.account = account
-        self.contract_dir = contract_dir
         self.expiration_sec = expiration_sec
         self.skip_signature = skip_signature
         self.dont_broadcast = dont_broadcast
@@ -135,37 +119,23 @@ class Contract(front_end.Logger):
         self.max_cpu_usage = max_cpu_usage
         self.max_net_usage = max_net_usage
         self.ref_block = ref_block
-        self.is_mutable = True
         self.verbosity = verbosity
         self.contract = None
         self._console = None
         self.error = self.account.error
-
-        front_end.Logger.__init__(self, verbosity)
-        self.INFO('''
-                ######### Create a ``Contract`` object.
-                ''')
-        config = teos.GetConfig(self.contract_dir, is_verbose=0)
-        self.contract_dir = config.json["contract-dir"]
-        if not self.contract_dir:
-            self.ERROR("""
-                Cannot determine the contract directory. The clue is 
-                ``{}``.
-                """.format(contract_dir))
-            return
         self.abi_file = abi_file
         self.wasm_file = wasm_file
-
-        self.INFO('''
-                * Contract directory is
-                    {}
-                '''.format(self.contract_dir))
 
     def is_built(self):
         return cleos.contract_is_built(
             self.contract_dir, self.wasm_file, self.abi_file)
 
     def deploy(self, permission=None, dont_broadcast=None):
+        if not self.is_built():
+            self.ERROR('''
+            Contract needs to be built before deployment.
+            ''')
+            return
         if dont_broadcast is None:
             dont_broadcast = self.dont_broadcast
         result = cleos.SetContract(
@@ -183,9 +153,9 @@ class Contract(front_end.Logger):
                 is_code = self.account.is_code()
                 if not is_code:
                     self.ERROR('''
-                Error in contract deployment:
-                Despite the ``set contract`` command returned without any error,
-                the code hash of the associated account is null
+                    Error in contract deployment:
+                    Despite the ``set contract`` command returned without any error,
+                    the code hash of the associated account is null.
                     ''')
                     return
                 else:
@@ -193,9 +163,8 @@ class Contract(front_end.Logger):
                     * The contract {} deployed. 
                     '''.format(self.contract_dir))
                     self.TRACE('''
-                    * Code hash is cross-checked not null.
+                    * Code hash is cross-checked to be non-zero.
                     ''')
-            # print(eosf.accout_names_2_object_names(json.dumps(result.json, indent=3)))
             self.contract = result
         else:
             self.contract = None
@@ -204,21 +173,6 @@ class Contract(front_end.Logger):
         if not self.contract:
             return False
         return not self.contract.error
-
-    def build_wast(self, json=False):
-        return ContractBuilder(
-            self.contract_dir,
-            self.is_mutable, self.verbosity).build_wast(json)
-
-    def build_abi(self, json=False):
-        return ContractBuilder(
-            self.contract_dir,
-            self.is_mutable, self.verbosity).build_abi(json)
-
-    def build(self):
-        return ContractBuilder(
-            self.contract_dir,
-            self.is_mutable, self.verbosity).build()
 
     def push_action(
             self, action, data,
@@ -261,16 +215,6 @@ class Contract(front_end.Logger):
             return str(self.contract.contract_path_absolute)
         else:
             return str(self.contract_dir)
-
-    def delete(self):
-        try:
-            if self.contract:
-                shutil.rmtree(str(self.contract.contract_path_absolute))
-            else:
-                shutil.rmtree(str(self.contract_dir))
-            return True
-        except:
-            return False
 
     def __str__(self):
         if self.is_deployed():
