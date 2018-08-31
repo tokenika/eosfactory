e03206d0824955557d41c1ad108c6c62b2ff2939
jakub-zarembinski <jakub.zarembinski@tokenika.io>
2018-08-31 17:50:52 +0200

    bold rm

--- pyteos/front_end.py
+++ pyteos/front_end.py
@@ -70,9 +70,9 @@ RAM needed is {}kB, deficiency is {}kB.
 
 class Verbosity(enum.Enum):
     COMMENT = ['green', None, []]
-    INFO = ['blue', None, ['bold']]
-    TRACE = ['cyan', None, ['bold']]
-    ERROR = ['red', None, ['reverse','bold']]
+    INFO = ['blue', None, []]
+    TRACE = ['cyan', None, []]
+    ERROR = ['red', None, ['reverse']]
     ERROR_TESTING = ['green', None, ['reverse']]
     OUT = [None, None, []]
     DEBUG = ['yellow', None, []]
