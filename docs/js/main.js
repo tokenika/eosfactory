48d6d897bf9cd140b2fd443ac58a3ef16ed94984
tokenika <contact@tokenika.io>
2018-09-01 16:07:35 +0200

    website rm

--- docs/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-  // get year for footer
-  var thisYear = new Date();
-      thisYear = thisYear.getFullYear();
-  $('.current-year').html(thisYear);
-
-  $(".preloader").fadeOut();
-});
