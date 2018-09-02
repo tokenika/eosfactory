a77b93f0ecc00e355272c9a479d4f79aa5951d64
tokenika <contact@tokenika.io>
2018-09-03 01:24:18 +0200

    to be removed

--- pyteos/eosfactory.py
+++ pyteos/eosfactory.py
@@ -26,19 +26,19 @@ contract_workspace_from_template = \
      eosf_contract.contract_workspace_from_template
 
 set_is_testing_errors = front_end.set_is_testing_errors
-set_is_throwing_errors = front_end.set_is_throwing_errors
+set_is_throwing_errors = front_end.set_is_throwing_errors #to be removed
 
 restart = eosf_account.restart
 
-resume = eosf.resume
 reset = eosf.reset
+resume = eosf.resume
 stop = eosf.stop
 
 info = eosf.info
 status = eosf.status
 
-verify_testnet_production = eosf.verify_testnet_production
-set_nodeos_address = setup.set_nodeos_address
+verify_testnet_production = eosf.verify_testnet_production #to be removed
+set_nodeos_address = setup.set_nodeos_address #to be removed
 
 Testnet = testnet_data.Testnet
 LocalTestnet = testnet_data.LocalTestnet
