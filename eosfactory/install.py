4287e5ade3909f7d5fd7c66328653f7171c2d791
jakub-zarembinski <jakub.zarembinski@tokenika.io>
2018-10-16 14:50:18 +0200

    install sequence fixed

--- eosfactory/install.py
+++ eosfactory/install.py
@@ -1,9 +1,15 @@
 import os
 from termcolor import cprint, colored
 import argparse
+import pathlib
 
 import eosfactory.core.utils as utils
 import eosfactory.core.config as config
+
+
+def tilde(tilde_path):
+    return tilde_path.replace("~", str(pathlib.Path.home()))
+
 
 def install(wsl_root):
     if wsl_root:
@@ -11,73 +17,81 @@ def install(wsl_root):
         map[config.wsl_root_[0]] = wsl_root
         config.write_config_map(map)
     
-    current_color = "green"
-    map = config.config_map()
-    if config.contract_workspace_[0] in map:
-        contract_workspace = input(utils.heredoc('''
-            Where are kept contract projects?
-            The current place is 
-                {}
-            Input another existing directory path, or nothing to keep the current one:
-            ''').format(colored(
-                    map[config.contract_workspace_[0]], 
-                    current_color)) + "\n\t")
-    else:
-        contract_workspace = input(utils.heredoc('''
-            Where are kept contract projects?
-            Input an existing directory path:
-            ''') + "\n\t")
+    current_path_color = "green"
+    error_path_color = "red"
+
     while True:
-        if not contract_workspace: #/mnt/c/Workspaces/EOS/contracts
+        map = config.config_map()
+        eosio_repository_dir = None
+
+        if config.eosio_repository_dir_[0] in map:
+            eosio_repository_dir = map[config.eosio_repository_dir_[0]]
+            _eosio_repository_dir = tilde(input(utils.heredoc('''
+                Where is the EOSIO repository located on your machine?
+                The current location is:
+                {}
+                Input another existing directory path, or nothing to keep the current one:
+                ''').format(colored(eosio_repository_dir, current_path_color)) + "\n"))
+        else:
+            _eosio_repository_dir = tilde(input(utils.heredoc('''
+                Where is the EOSIO repository located on your machine?
+                Input an existing directory path:
+                ''') + "\n"))
+
+        if not _eosio_repository_dir:
+            _eosio_repository_dir = eosio_repository_dir
+
+        ok = os.path.exists(
+            os.path.join(_eosio_repository_dir, "build/programs/nodeos/nodeos")) and \
+            os.path.exists(
+            os.path.join(_eosio_repository_dir, "build/programs/cleos/cleos"))
+
+        if ok:
+            map = config.config_map()
+            map[config.eosio_repository_dir_[0]] = _eosio_repository_dir
+            config.write_config_map(map)
+            print()
+            break
+
+        print("\n" + utils.heredoc('''
+        The path you entered:
+        {}
+        doesn't seem to be correct! EOSIO executables are not detected there.
+        ''').format(colored(_eosio_repository_dir, error_path_color)) + "\n")
+
+    while True:
+        map = config.config_map()
+        contract_workspace_dir = None
+
+        if config.contract_workspace_[0] in map:
+            contract_workspace_dir = map[config.contract_workspace_[0]]
+            _contract_workspace_dir = tilde(input(utils.heredoc('''
+                Where do you prefer to keep your smart-contract projects?
+                The current location is:
+                {}
+                Input another existing directory path, or nothing to keep the current one:
+                ''').format(colored(contract_workspace_dir, current_path_color)) + "\n"))
+        else:
+            _contract_workspace_dir = tilde(input(utils.heredoc('''
+                Where do you prefer to keep your smart-contract projects?
+                Input an existing directory path:
+                ''') + "\n"))
+
+        if not _contract_workspace_dir:
+            _contract_workspace_dir = contract_workspace_dir
+        
+        if os.path.exists(_contract_workspace_dir) and os.path.isdir(_contract_workspace_dir):
+            map = config.config_map()
+            map[config.contract_workspace_[0]] = _contract_workspace_dir
+            config.write_config_map(map)
             print()
             break
         
-        if contract_workspace:
-            if os.path.exists(contract_workspace) and os.path.isdir(contract_workspace):
-                map = config.config_map()
-                map[config.contract_workspace_[0]] = contract_workspace
-                config.write_config_map(map)
-                print()
-                break
-            else:
-                print(utils.heredoc('''
-            {}
-            is not an existing directory path.
-                ''').format(colored(contract_workspace), current_color))
-
-    map = config.config_map()
-    if config.eosio_repository_dir_[0] in map: #/mnt/c/Workspaces/EOS/eos
-        eosio_repository_dir = input(utils.heredoc('''
-            Where is an eosio repository?
-            The current place is 
-                {}
-            Input another existing directory path, or nothing to keep the current one:
-            ''').format(colored(
-                map[config.eosio_repository_dir_[0]],
-                current_color)) + "\n\t")
-    else:
-        eosio_repository_dir = input(utils.heredoc('''
-            Where is an eosio repository?
-            Input an existing directory path:
-            ''') + "\n\t")
-    while True:
-        if not eosio_repository_dir:
-            print()
-            break
-
-        if eosio_repository_dir:
-            config.eosio_repository_dir_[1][0] = eosio_repository_dir
-            if config.node_exe() and  config.cli_exe():
-                map = config.config_map()
-                map[config.eosio_repository_dir_[0]] = eosio_repository_dir
-                config.write_config_map(map)
-                print()
-                break
-        else:
-            print(utils.heredoc('''
-            {}
-            is not a satisfactory path as the eosio executables are not in their places.
-                ''').format(colored(contract_workspace), current_color))
+        print("\n" + utils.heredoc('''
+        The path you entered:
+        {}
+        doesn't seem to exist!
+        ''').format(colored(_contract_workspace_dir, error_path_color)) + "\n")
 
         
 parser = argparse.ArgumentParser(description='''
