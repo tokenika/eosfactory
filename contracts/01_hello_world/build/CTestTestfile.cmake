1d6273481ed017ea18988c2ebb1f2fc425c13416
jakub-zarembinski <jakub.zarembinski@tokenika.io>
2018-12-07 17:04:30 +0100

    rm build files

--- contracts/01_hello_world/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Workspaces/EOS/contracts/01_hello_world
-# Build directory: /mnt/c/Workspaces/EOS/contracts/01_hello_world/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests "python3" "/mnt/c/Workspaces/EOS/contracts/01_hello_world/tests/test1.py")
-add_test(unittest "python3" "/mnt/c/Workspaces/EOS/contracts/01_hello_world/tests/unittest1.py")
