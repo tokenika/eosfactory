d8ed410d5659b64080e6f8bc24e46d554fb4dd92
Krzysztof Szumny <noisy.pl@gmail.com>
2019-01-13 03:30:24 +0100

    fix for "Cannot find any path for 'EOSIO_GENESIS_JSON'."

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='eosfactory',
-      version='0.0.19',
+      version='0.0.24',
       description='Python-based EOS smart-contract development & testing framework',
       long_description=readme(),
       classifiers=[
@@ -23,6 +23,7 @@ setup(name='eosfactory',
       author_email='contact@tokenika.io',
       license='MIT',
       packages=find_packages(),
+      include_package_data=True,
       install_requires=[
           'termcolor',
       ],
