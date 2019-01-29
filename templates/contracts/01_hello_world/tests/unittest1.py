c6dc481cd9aaa7cb57843fa3cd19fb45785fd3f6
Ryan Lee <drdgvhbh@gmail.com>
2019-01-28 20:37:59 -0500

    Update unittest1.py

--- templates/contracts/01_hello_world/tests/unittest1.py
+++ templates/contracts/01_hello_world/tests/unittest1.py
@@ -1,15 +1,16 @@
-import unittest, sys
+import unittest
+import sys
 from eosfactory.eosf import *
 
 verbosity([Verbosity.INFO, Verbosity.OUT])
 
 CONTRACT_WORKSPACE = sys.path[0] + "/../"
 
+
 class Test(unittest.TestCase):
 
     def run(self, result=None):
         super().run(result)
-
 
     @classmethod
     def setUpClass(cls):
@@ -17,52 +18,49 @@ class Test(unittest.TestCase):
         Execute simple actions.
         ''')
         reset()
-        create_master_account("master")
+
+        cls.master = create_master_account("master")
 
         COMMENT('''
         Build and deploy the contract:
         ''')
-        create_account("host", master)
-        contract = Contract(host, CONTRACT_WORKSPACE)
+        cls.host = create_account("host", cls.master)
+        contract = Contract(cls.host, CONTRACT_WORKSPACE)
         contract.build(force=False)
         contract.deploy()
 
         COMMENT('''
         Create test accounts:
         ''')
-        create_account("alice", master)
-        create_account("carol", master)
-        create_account("bob", master)
-
+        cls.alice = create_account("alice", cls.master)
+        cls.carol = create_account("carol", cls.master)
+        cls.bob = create_account("bob", cls.master)
 
     def setUp(self):
         pass
-
 
     def test_01(self):
         COMMENT('''
         Test an action for Alice:
         ''')
-        host.push_action(
-            "hi", {"user":alice}, permission=(alice, Permission.ACTIVE))
+        self.host.push_action(
+            "hi", {"user": self.alice}, permission=(self.alice, Permission.ACTIVE))
 
         COMMENT('''
         Test an action for Carol:
         ''')
-        host.push_action(
-            "hi", {"user":carol}, permission=(carol, Permission.ACTIVE))
+        self.host.push_action(
+            "hi", {"user": self.carol}, permission=(self.carol, Permission.ACTIVE))
 
         COMMENT('''
         WARNING: This action should fail due to authority mismatch!
         ''')
         with self.assertRaises(MissingRequiredAuthorityError):
-            host.push_action(
-                "hi", {"user":carol}, permission=(bob, Permission.ACTIVE))
-
+            self.host.push_action(
+                "hi", {"user": self.carol}, permission=(self.bob, Permission.ACTIVE))
 
     def tearDown(self):
         pass
-
 
     @classmethod
     def tearDownClass(cls):
