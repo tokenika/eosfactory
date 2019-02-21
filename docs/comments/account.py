b33ed9445bdbfb5f7e31c698b8fb9738667e024f
Stefan Zarembinski <stefan.zarembinski@gmail.com>
2019-02-21 20:39:54 +0100

    restoration point

---
+++ docs/comments/account.py
@@ -0,0 +1,24 @@
+
+from eosfactory.eosf import *
+
+reset()
+create_master_account("master")
+
+create_account("host", master)
+
+host.info()
+
+contract = Contract(host, "hello_world")
+
+contract.build()
+contract.deploy()
+
+create_account("alice", master)
+create_account("carol", master)
+
+host.push_action("hi", {"user":alice}, alice)
+host.push_action("hi", {"user":carol}, carol)
+
+host.show_action("hi", {"user":alice}, alice)
+
+stop()
