5bd6048fa3d5b03a4b0a8acdd60499093537763f
Stefan Zarembinski <stefan.zarembinski@gmail.com>
2019-02-22 15:22:04 +0100

    Update all_tests.sh

--- tests/all_tests.sh
+++ tests/all_tests.sh
@@ -7,10 +7,10 @@ python3 tests/new_eosio_token.py &&\
 python3 tests/tic_tac_toe.py &&\
 python3 tests/new_tic_tac_toe.py &&\
 
-eosfactory/pythonmd.sh docs/cases/account.md &&\
-eosfactory/pythonmd.sh docs/cases/master_account.md &&\
-eosfactory/pythonmd.sh docs/cases/symbolic_names.md &&\
-eosfactory/pythonmd.sh docs/cases/wallet.md &&\
+eosfactory/pythonmd.sh docs/comments/account.md &&\
+eosfactory/pythonmd.sh docs/comments/master_account.md &&\
+eosfactory/pythonmd.sh docs/comments/symbolic_names.md &&\
+eosfactory/pythonmd.sh docs/comments/wallet.md &&\
 eosfactory/pythonmd.sh docs/patterns/set/set_account_permission.md &&\
 eosfactory/pythonmd.sh docs/tutorials/02.InteractingWithEOSContractsInEOSFactory.md &&\
 
