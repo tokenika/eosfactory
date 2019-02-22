f9503f598fde1900f6de65675431ad46a326b8ec
Stefan Zarembinski <stefan.zarembinski@gmail.com>
2019-02-22 21:59:26 +0100

    restoration point

--- docs/comments/master_account.py
+++ docs/comments/master_account.py
@@ -1,29 +1,29 @@
 
-from eosfactory.eosf import *
+# from eosfactory.eosf import *
 
-reset()
+# reset()
 
-create_master_account("master")
-master.info()
+# create_master_account("MASTER")
+# MASTER.info()
 
-create_account("alice", master)
-alice.info()
-
-stop()
-reboot()
+# create_account("ALICE", MASTER)
+# ALICE.info()
 
 from eosfactory.eosf import *
 
 testnet = Testnet(
-    "dgxo1uyhoytn", "5JE9XSurh4Bmdw8Ynz72Eh6ZCKrxf63SmQWKrYJSXf1dEnoiKFY", "5JgLo7jZhmY4huDNXwExmaWQJqyS1hGZrnSjECcpWwGU25Ym8tA",
-    "http://88.99.97.30:38888")
+    "nukjygmgkn3x",
+    "5KXxczFPdcsLrCYpRRREfd4e2xVDTZZqBpZWmvxLZYxUbPzqrWL",
+    "5KJLMupynNYFiM9gZWtDnDX55hbaF18EsWpFr8UvyJeADqbwN7A",
+    "http://145.239.133.201:8888"
+    )
 
 testnet.configure()
 testnet.verify_production()
 testnet.clear_cache()
 
-create_master_account("master", testnet)
-master.info()
+create_master_account("MASTER", testnet)
+MASTER.info()
 
-create_account("carol", master, buy_ram_kbytes=8, stake_net=3, stake_cpu=3)
-carol.info()
+create_account("CAROL", MASTER, buy_ram_kbytes=8, stake_net=3, stake_cpu=3)
+CAROL.info()
