78aca3bf08cf35a43a2a5299aebcf204ddb024f4
Stefan Zarembinski <stefan.zarembinski@gmail.com>
2019-04-10 12:20:59 +0200

    all tested to pypi

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Tokenika'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '3.0.4'
+release = '3.0.5'
 
 
 # -- General configuration ---------------------------------------------------
