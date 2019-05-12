d80ac9d77ae6b56261329cff8da68108a1d8c534
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2019-05-12 14:58:29 +0300

    fix logger for account permission

--- eosfactory/shell/account.py
+++ eosfactory/shell/account.py
@@ -200,7 +200,7 @@ class Account():
         '''
         stop_if_account_is_not_set(self)
         logger.TRACE('''
-            * Set action permission.
+            * Set account permission.
             ''')
         result = cleos_set.SetAccountPermission(
                 self, permission_name, authority, parent_permission_name,
